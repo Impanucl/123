e8669804f0e70efb57d711e800f69d131bc8d30c
Impanucl <andrey.atavin46@gmail.com>
2018-01-23 00:50:35 +0300

    new 2D game

---
+++ Study_with_book/memory/Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+    [SerializeField] private GameObject cardBack;
+    public void OnMouseDown()
+    {
+       if (cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
