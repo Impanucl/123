32908f46889f6f59cb203ec63e359b7f95ebb7e4
Impanucl <andrey.atavin46@gmail.com>
2018-01-30 02:06:00 +0300

    enemyAI

--- Study_with_book/First_doom/Assets/Tanks_Game/Scripts/CannonFire.cs
+++ Study_with_book/First_doom/Assets/Tanks_Game/Scripts/CannonFire.cs
@@ -15,15 +15,13 @@ public class CannonFire : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         reloadTime -=Time.deltaTime;
-        Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;
 
         if (Input.GetButtonDown("Jump") && reloadTime <=0)
         {
             _cannonPref = Instantiate(CannonPrefab) as GameObject;
             _cannonPref.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
             _cannonPref.transform.rotation = transform.rotation;
-            reloadTime += 0.0f;
+            reloadTime += 2.0f;
         }
 	}
 }
---
+++ Study_with_book/First_doom/Assets/Tanks_Game/Scripts/EnemyAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+    private bool _alive;
+    public float speed = 3.0f;
+    public float StopRange = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        _alive = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_alive)
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+
+            Ray ray = new Ray(transform.position, -transform.right);
+            RaycastHit hit;
+
+            if(Physics.SphereCast(ray, 0.75f, out hit))
+            {
+                if(hit.distance < StopRange)
+                {
+                    float angle = Random.Range(-110, 110);
+                    transform.Rotate(0, 0, angle);
+                    GameObject hitObject = hit.transform.gameObject;
+                        Debug.Log(hitObject);
+                }
+            }
+        }
+	}
+
+    public void SetAlive(bool alive)
+    {
+        _alive = alive;
+    }
+}
