c9b1b8de2a1435178a46f5fbd641bec01f2eee5c
Impanucl <andrey.atavin46@gmail.com>
2018-02-19 11:40:05 +0300

    Memory finish

---
+++ Study_with_book/memory/Assets/Scripts/UIButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour {
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private string targetMessage;
+    public Color highlightColor = Color.cyan;
+
+    public void OnMouseOver() {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null){
+            sprite.color = highlightColor;
+        }
+    }
+
+    public void OnMouseExit(){
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null){
+            sprite.color = Color.white;
+        }
+    }
+
+    public void OnMouseDown(){
+        transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+    }
+
+    public void OnMouseUp()
+    {
+        transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        if (targetObject != null) {
+            targetObject.SendMessage(targetMessage);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
