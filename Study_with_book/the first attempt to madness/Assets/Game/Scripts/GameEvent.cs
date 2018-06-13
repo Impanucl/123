718fd794cfb3cc8e9a6be94c097b328e263fb83a
nucl <a.atavin@it.ecommpay.com>
2018-06-13 22:49:31 +0300

    3d to 2d

--- Study_with_book/the first attempt to madness/Assets/Game/Scripts/GameEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class GameEvent {
-    public const string ON_TAP_ROTATION        = "ON_TAP_ROTATION";
-    public const string SPAWN_BULLET           = "SPAWN_BULLET";
-    public const string ON_TEMPERATURE_COOLING = "ON_TEMPERATURE_COOLING";
-}
