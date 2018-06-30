4381932f9fa58d3beb3fd70e493c024ead34625b
nucl <a.atavin@it.ecommpay.com>
2018-06-30 21:29:42 +0300

    graphics

---
+++ Study_with_book/second attempt to madness/Assets/Scripts/FPSInputs.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSInputs : MonoBehaviour {
+	[SerializeField] private CharacterController _charController;
+	public float speed = 6.0f;
+	[SerializeField] public Joystick moveJoystick;
+
+	private float deltaX = 0;
+	private float deltaY = 0;
+
+	public enum moveAxes
+	{
+		Joystick = 0,
+		Keyboard = 1
+	}
+
+	public moveAxes axes = moveAxes.Joystick;
+
+	void Start () {
+		_charController = GetComponent<CharacterController>();
+	}
+		
+	void Update () {
+		
+		if (axes == moveAxes.Joystick) {
+			//управление джойстиком
+			 deltaX = moveJoystick.Horizontal * speed;
+			 deltaY = moveJoystick.Vertical * speed;
+		} else {
+			//управление клавиатурой
+			 deltaX = Input.GetAxis("Horizontal") * speed;
+			 deltaY = Input.GetAxis("Vertical") * speed;
+		}
+
+		Vector3 movement = new Vector3(deltaX, deltaY, 0);
+		movement = Vector3.ClampMagnitude(movement, speed);
+		movement *= Time.deltaTime;
+
+		if (deltaX != 0 || deltaY != 0)
+		{
+			//эти строки позволяют при движении поворачивать корпус плейера, для 3d 
+			//	Quaternion look = Quaternion.LookRotation(movement);
+			//	transform.rotation = Quaternion.Slerp(transform.rotation, look, Time.deltaTime * speed/5);
+			_charController.Move(movement);
+		}
+	}
+
+	//вычисление 
+	public float SetPlayerPosition(float Ox, float Oy){
+			
+	}
+
+}
--- Study_with_book/second attempt to madness/Assets/Scripts/MoveJoystick.cs
+++ Study_with_book/second attempt to madness/Assets/Scripts/MoveJoystick.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MoveJoystick : Joystick
+public class MoveJoystick : Joystick, FPSInputs
 {
     [Header("Options")]
 
     Vector2 joystickCenter = Vector2.zero;
+	private float Xposition = 0;
+	private float Ypostion  = 0;
+
+	private MoveJoystick moveJoystick;
 
     void Start()
     {
@@ -23,12 +27,16 @@ public class MoveJoystick : Joystick
         Vector2 direction = eventData.position - joystickCenter;
         inputVector = (direction.magnitude > background.sizeDelta.x / 2f) ? direction.normalized : direction / (background.sizeDelta.x / 2f);
         handle.anchoredPosition = (inputVector * background.sizeDelta.x / 2f) * handleLimit;
+
+		//передача координат джойстика для анимации движения плейера 
+		Xposition = handle.transform.localPosition.x;
+		Ypostion  = handle.transform.localPosition.y;
+		SetPositionNumber(Xposition, Yposition);
     }
 
     public override void OnPointerDown(PointerEventData eventData)
     {
         background.gameObject.SetActive(true);
-		Debug.Log ('нажал');
     }
 
     public override void OnPointerUp(PointerEventData eventData)
@@ -38,4 +46,12 @@ public class MoveJoystick : Joystick
         handle.anchoredPosition = Vector2.zero;
     }
 
+
+	public float SetPositionNumber(float positionX, float positionY)
+	{
+	 
+
+
+
+	}
 }
